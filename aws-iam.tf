d9efb31eb38243f149e28cb153c1208d94893cb0
Marc Schnitzius <marc.schnitzius@codecentric.de>
2024-03-13 11:49:33 +0100

    add aws iam user konstantin.bauer and allow ecr access

---
+++ aws-iam.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_user" "konstantin_bauer" {
+  name = "konstantin.bauer"
+}
+
+resource "aws_iam_policy" "ecr_policy" {
+  name        = "ecr_policy"
+  description = "ECR policy for read, pull, and get"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "ECRReadPullGet",
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:BatchGetImage"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "konstantin_bauer_policy_attachment" {
+  user       = aws_iam_user.konstantin_bauer.name
+  policy_arn = aws_iam_policy.ecr_policy.arn
+}
