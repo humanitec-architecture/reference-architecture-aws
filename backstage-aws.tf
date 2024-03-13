d7dddcd6866b0ce2d491506d93859e7190d0c711
Marc Schnitzius <marc.schnitzius@codecentric.de>
2024-03-13 11:15:39 +0100

    add petclinic ecr repository

--- backstage-aws.tf
+++ backstage-aws.tf
@@ -12,3 +12,15 @@ module "backstage_ecr" {
 
   repository_force_delete = true
 }
+
+module "petclinic_ecr" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "~> 1.6"
+
+  repository_name                 = "petclinic"
+  repository_image_scan_on_push   = false
+  repository_image_tag_mutability = "MUTABLE"
+  create_lifecycle_policy         = false
+
+  repository_force_delete = true
+}
