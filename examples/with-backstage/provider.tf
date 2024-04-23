a23c0cd07a56e79fee4d216e5c8054f0c3416c28
Johannes Würbach <johannes.wuerbach@googlemail.com>
2024-04-23 14:55:22 +0200

    feat: set backstage service-to-service auth key

--- examples/with-backstage/provider.tf
+++ examples/with-backstage/provider.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.38"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
   }
   required_version = ">= 1.3.0"
 }
